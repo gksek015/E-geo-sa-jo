9cfdfad7de68d8358c56915a92c6d8cb9e3a00c5
gksekdud <dayoung1490@gmail.com>
2024-12-02 21:00:27 +0900

    update: Login, Signup

--- src/components/auth/LoginComponent.jsx
+++ src/components/auth/LoginComponent.jsx
@@ -1,142 +1,46 @@
-// import React, { useState } from 'react';
-// import { Link } from 'react-router-dom';
-// import styled from 'styled-components';
-
-// const LoginComponent = () => {
-//   const [email, setEmail] = useState('');
-//   const [password, setPassWord] = useState('');
-
-//   const handleLogin = (e) => {
-//     e.preventDefault();
-//     console.log(email, password);
-//   };
-
-//   return (
-//     <Background>
-//       <Container>
-//         <Title>Login</Title>
-//         <Logo>#</Logo>
-//         <form onSubmit={handleLogin}>
-//           <Input type="email" placeholder="이메일" value={email} onChange={(e) => setEmail(e.target.value)} />
-//           <Input
-//             type="password"
-//             placeholder="비밀번호"
-//             value={password}
-//             onChange={(e) => setPassWord(e.target.value)}
-//           />
-//           <ButtonGroup>
-//             <Button type="submit">로그인</Button>
-//             <LinkButton to="/signup">회원가입</LinkButton>
-//           </ButtonGroup>
-//         </form>
-//       </Container>
-//     </Background>
-//   );
-// };
-
-// export default LoginComponent;
-
-// const Background = styled.div`
-//   width: 100vw;
-//   height: 100vh;
-//   background-color: var(--background--color);
-//   display: flex;
-//   justify-content: center;
-//   align-items: center;
-// `;
-
-// const Container = styled.div`
-//   width: 100%;
-//   max-width: 700px;
-//   padding: 32px;
-//   background-color: #fff;
-//   border-radius: 10px;
-//   box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-//   text-align: center;
-// `;
-
-// const Logo = styled.div`
-//   font-size: 48px;
-//   margin-bottom: 16px;
-//   color: var(--font--primary--color);
-// `;
-
-// const Title = styled.h1`
-//   font-size: 32px;
-//   color: var(--font--primary--color);
-//   margin-bottom: 32px;
-// `;
-
-// const Input = styled.input`
-//   width: 100%;
-//   padding: 8px;
-//   border: 1px solid var(--font--secondary--color);
-//   border-radius: 5px;
-//   font-size: 14px;
-//   color: var(--font--secondary--color);
-//   margin-bottom: 16px;
-
-//   &::placeholder {
-//     color: var(--font--secondary--color);
-//   }
-// `;
-
-// const Button = styled.button`
-//   width: 48%;
-//   padding: 8px;
-//   margin: 8px 0.5%;
-//   background-color: var(--button--color);
-//   color: #fff;
-//   font-size: 14px;
-//   border: none;
-//   border-radius: 5px;
-//   cursor: pointer;
-
-//   &:hover {
-//     background-color: transparent;
-//     color: var(--button--color);
-//     border: 1px solid var(--button--color);
-//     margin: 7px 0.5%;
-//   }
-// `;
-
-// const LinkButton = styled(Link)`
-//   width: 48%;
-//   padding: 8px;
-//   margin: 8px 0.5%;
-//   background-color: #fff;
-//   color: var(--button--color);
-//   font-size: 14px;
-//   text-align: center;
-//   border: 1px solid var(--button--color);
-//   border-radius: 5px;
-//   cursor: pointer;
-//   text-decoration: none;
-//   transition: background-color 0.1s ease-in-out, color 0.1s ease-in-out;
-
-//   &:hover {
-//     background-color: var(--button--color);
-//     color: #fff;
-//   }
-// `;
-
-// const ButtonGroup = styled.div`
-//   display: flex;
-//   justify-content: space-between;
-//   gap: 8px;
-// `;
-
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
+import supabase from '../../supabase/supabaseClient';
+import { toast } from 'react-toastify';
 
 const LoginComponent = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
 
-  const handleLogin = (e) => {
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    const {name, value} = e.target;
+    setFormData({...formData, [name]: value});
+  }
+
+  const handleLogin = async (e) => {
     e.preventDefault();
-    console.log(email, password);
+    const {email, password} = formData;
+
+    try {
+      const {data, error} = await supabase.auth.signInWithPassword({
+        email,
+        password,
+      });
+      console.log({data})
+
+      if (error) {
+        toast.error("로그인에 실패했습니다.")
+      }
+
+      if (data.user) {
+        toast.success("로그인 성공!");
+        navigate("/home");
+      } else {
+        toast.error("로그인에 실패했습니다.")
+      }
+    } catch (error) {
+      toast.error("알 수 없는 에러가 발생했습니다. 다시 시도해주세요.")
+    }
   };
 
 
@@ -151,12 +55,21 @@ const LoginComponent = () => {
           />
         </Logo>
         <form onSubmit={handleLogin}>
-          <Input type="email" placeholder="이메일" value={email} onChange={(e) => setEmail(e.target.value)} />
+          <Input
+          type="email"
+          name="email"
+          placeholder="이메일"
+          value={formData.email}
+          onChange={handleChange}
+          required
+          />
           <Input
             type="password"
+            name="password"
             placeholder="비밀번호"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
+            value={formData.password}
+            onChange={handleChange}
+            required
           />
           <ButtonGroup>
             <Button type="submit">로그인</Button>
