f85a8cb8aaaace102fea5b3adb4b4a9ee03c993d
bj9322 <bj932218@gmail.com>
2024-12-02 14:51:22 +0900

    📝라우터 재설정 (프라이빗, 퍼블릭)

--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import PublicRoute from './PublicRoute'; 
+import PrivateRoute from './PrivateRoute'; 
 import LoginPage from '../pages/LoginPage';
 import SignupPage from '../pages/SignupPage';
-// import Layout from '../components/layout/Layout';
 import HomePage from '../pages/HomePage';
 import MyPage from '../pages/MyPage';
 import MyProfilePage from '../pages/MyProfilePage';
@@ -15,18 +16,21 @@ const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<LoginPage />} />
-        <Route path="signup" element={<SignupPage />} />
-        {/* <Route element={<Layout />}> */}
-          <Route path="home" element={<HomePage />} />
+        <Route element={<PublicRoute />}>
+          <Route path="/" element={<LoginPage />} />
+          <Route path="signup" element={<SignupPage />} />
+        </Route>
+
+        <Route element={<PrivateRoute />}>
+          <Route path="/" element={<HomePage />} />
           <Route path="home/:id" element={<DetailPage />} />
           <Route path="mypage" element={<MyPage />} />
           <Route path="mypage/:id" element={<UpdatePostPage />} />
           <Route path="myprofile" element={<MyProfilePage />} />
           <Route path="post" element={<PostPage />} />
-          <Route path='list' element={<ListPage/>}/>
-          <Route path='list/:id' element={<DetailPage/>}/>
-        {/* </Route> */}
+          <Route path="list" element={<ListPage />} />
+          <Route path="list/:id" element={<DetailPage />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
