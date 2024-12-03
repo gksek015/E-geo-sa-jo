f16e853a5965080bcc672efc8033d8a423c0c584
Raina-Moon <mds6425@gmail.com>
2024-12-03 23:19:28 +0900

    📝 헤더푸터 중복되는거 빼버림

--- src/pages/PostPage.jsx
+++ src/pages/PostPage.jsx
@@ -1,14 +1,10 @@
 import React from 'react';
 import PostForm from '../components/posting/PostForm';
-import Header from '../components/layout/Header';
-import Footer from '../components/layout/Footer';
 
 const PostPage = () => {
   return (
     <div>
-      <Header />
         <PostForm />
-      <Footer />
     </div>
   );
 
